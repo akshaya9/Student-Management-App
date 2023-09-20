b1e19dd304db7679f921c56322a245e1773b87ac
Akshaya Sunkari <sunkariakshaya3890@gmail.com>
2023-09-19 20:06:51 -0700

    backend commit

---
+++ StudentApplicationbackend/src/main/java/com/akshaya/StudentApplication/model/Student.java
@@ -0,0 +1,42 @@
+package com.akshaya.StudentApplication.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+    @Id  //    primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  //     Make it auto-incremented
+    private int id;
+    private String name;
+    private String addr;
+
+    public Student() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+}
